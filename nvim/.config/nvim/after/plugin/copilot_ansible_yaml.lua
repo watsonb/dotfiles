6d50abc62dbf304de1c311d3559d5b1cd9cf75ce
Ben Watson <bwatson1979@gmail.com>
2026-05-13 20:13:52 -0500

    fix: disable copilot limit reached message

--- nvim/.config/nvim/after/plugin/copilot_ansible_yaml.lua
+++ nvim/.config/nvim/after/plugin/copilot_ansible_yaml.lua
@@ -3,4 +3,5 @@ require("copilot").setup({
     ["yaml.ansible"] = true, -- hail mary?
     ["ansible"] = true, -- in the event we have/use an ansible filetype
   },
+  disable_limit_reached_message = true,
 })
