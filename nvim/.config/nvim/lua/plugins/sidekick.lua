850f572cec124a4c590ddf3aa9787fae253fc770
Ben Watson <benjamin.watson@kiewit.com>
2026-07-01 08:03:11 -0500

    fix: only disable the tmux portion for now.

--- nvim/.config/nvim/lua/plugins/sidekick.lua
+++ nvim/.config/nvim/lua/plugins/sidekick.lua
@@ -2,17 +2,28 @@ return {
   {
     "folke/sidekick.nvim",
     opts = {
-      -- cli = {
-      --   mux = {
-      --     enabled = true,
-      --     backend = "tmux",
-      --   },
-      --   win = {
-      --     split = {
-      --       width = 60,
-      --     },
-      --   },
-      -- },
+      cli = {
+        -- mux = {
+        --   enabled = true,
+        --   backend = "tmux",
+        -- },
+        win = {
+          split = {
+            width = 80,
+          },
+        },
+        tools = {
+          copilot = {
+            native_scroll = true,
+          },
+          claude = {
+            native_scroll = true,
+          },
+          gemini = {
+            native_scroll = true,
+          },
+        },
+      },
       copilot = {
         status = {
           level = vim.log.levels.OFF,
